3acec623abfc24c35ad656372678c7b47ded5720
Ricardo <ricardosoli85@gmail.com>
2024-08-19 22:48:15 -0300

    atual

--- data/RGP.jl
+++ data/RGP.jl
@@ -1,12 +1,14 @@
 # Parâemtros exemplo Robusto Kutcha 
 
 A = [3 7 5;6 5 7;3 6 5;-28 -40 -32]; # matriz A
-a = [0.3 0.7 0.5;0.6 0.5 0.7;0.3 0.6 0.5;-2.8 -4.0 -3.2]
+a = [0.3 0.7 0.5;0.6 0.5 0.7;0.3 0.6 0.5;2.8 4.0 3.2]
 b = [200; 200; 200;-1500];         # demanda 
 c = [1 1 1 1];  
-# parametros 
-gama =[1, 1, 1, 1]                # custo 
-(m,n) = size(A)
 
-include("modeloMRob.jl")
-linear_robusto(A,b,c,m)
+
+# parametros 
+Gamma = [[0,0,0,0],[0,0,0,3],[1,1,1,1],[1,1,1,3],[3,3,3,3]]
+for gama in Gamma 
+    include("modeloMRob.jl")
+    resultados  = linear_robusto(A,b,c,gama)
+end 
--- data/modeloMRob.jl
+++ data/modeloMRob.jl
@@ -8,7 +8,7 @@ using Gurobi
 
 # Função para resolver o modelo 
 
-function linear_robusto(A,b,c,m)
+function linear_robusto(A,b,c,gama)
     # criar o modelo
     modelo = Model(Gurobi.Optimizer)
     
@@ -36,43 +36,75 @@ function linear_robusto(A,b,c,m)
     sum(p[i,j] for j in 1:n)- 
     pos[i] + 
     neg[i] + 
-    gama[i]*z[i]
-    ==b[i])
+    gama[i]*z[i] ==b[i])
 
     @constraint(modelo, rest2[i in 1:m,  j in 1:n],
-    z[i] + p[i,j] >= 
-    a[i,j]*x[j])
+    z[i] + p[i,j] >=   a[i,j]*x[j])
  
     
  
     # resolver 
     optimize!(modelo)
 
+    # Função para salvar os arquivos 
+
+     function salvar_resultados(resultados, arquivo_resultados)
+        resultado_arquivo = open(arquivo_resultados,"a")
+        println(resultado_arquivo,resultados)
+        close(resultado_arquivo)        
+     end
+
      # Verificar a solução
+    resultados = "Impresão dos resultados \n"
+    resultados *= "Modelo : RGP \n"
+    resultados *= "Valores de Gamma = $gama \n"
+    resultados *= "--------Status da Solução-------\n"
 
     if termination_status(modelo) == MOI.OPTIMAL
         println("Solução ótima encontrada")
         println("Status = ", termination_status(modelo))
-        println("Objetivo = ", objective_value(modelo))
+        println("Objetivo = ", objective_value(modelo)) 
+        
+        #resultados *= "Status: Solução Ótima\n" 
+        resultados *= "[Objetivo] = $(objective_value(modelo))\n"
+        resultados *= "---------------------------------------\n"
+        resultados *= "Status =  $(termination_status(modelo))\n"
+        resultados *= "Tempo Solução  = $(solve_time(modelo))\n" 
         
 
         println("")
         
         for i in 1:n
             println("x[$i] = ", value(x[i]))
+            resultados *= "x[$i] = $(value(x[i]))\n"
         end
-        
+        resultados *= "...................................\n"
         for j in 1:m
             println("pos[$j] = ", value(pos[j]))
             println("neg[$j] = ", value(neg[j]))
+            resultados *= "pos[$j] = $(value(pos[j]))\n"
+            resultados *= "neg[$j] = $(value(neg[j]))\n"
+            
         end
-        return modelo 
-        #return value.(x), value.(pos), value.(neg), objective_value(modelo)
+         resultados *= "*-Fim do resultados do Teste-*\n "
+      
     else
         println("Solução não ótima ou erro")
         return nothing, nothing, nothing, nothing
-    end
 
- 
+        salvar_resultados(resultados, "resultados_1.txt")
+    end
+    
+    
+    salvar_resultados(resultados, "resultados_1.txt")
+
+    # Função para salvar os resultados em um arquivo
+    function salvar_resultados(resultados, arquivo_resultados)
+        open(arquivo_resultados, "a") do resultado_arquivo
+            println(resultado_arquivo, resultados)
+        end
+end
+
+  
 
 end 
