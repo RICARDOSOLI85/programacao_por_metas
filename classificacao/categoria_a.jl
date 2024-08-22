3c1b570f5a8b52157fca08fd8e077131b46f3c3a
Ricardo <ricardosoli85@gmail.com>
2024-08-22 16:08:23 -0300

    função select para retirar a primeira coluna e o label do data set

--- classificacao/categoria_a.jl
+++ classificacao/categoria_a.jl
@@ -13,21 +13,11 @@ function dividir_categorias(df_treino::DataFrame)
     ca_treino = filter(:COVID => n -> n == 1.0, df_treino)
     cb_treino = filter(:COVID => n -> n == 0.0, df_treino)
 
-
-    
-    #=
-    #ca_treino = df_treino[df_treino[:, :COVID] .== 1, :]
-    #cb_treino = df_treino[df_treino[:, :COVID] .== 0, :]
-    ca_treino = filter(row -> row[:COVID] > 0, df_treino) # Filtra linhas onde COVID > 0
-    cb_treino = filter(row -> row[:COVID] < 1, df_treino) # Filtra linhas onde COVID < 1 
-    
     # Remover a coluna COVID de ambos data frames e a coluna inicial 
-    #ca = ca_treino[:,2:14]
-    #b = cb_treino[:,2:14]
-    #C  = c_treino[:,2:14] =#
-    ca = ca_treino 
-    cb = cb_treino 
-    C = df_treino
+    # usando a função select do DataFrames 
+    ca = select(ca_treino, Not([:Column1, :COVID]))
+    cb = select(cb_treino, Not([:Column1, :COVID]))
+    C = select(df_treino, Not([:Column1, :COVID]))
     return C, ca, cb
     
 
