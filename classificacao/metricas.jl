9fa526c587c358d5ae9b223ea2abcb330e707527
Ricardo <ricardosoli85@gmail.com>
2024-08-22 23:31:10 -0300

    atualizado metricas e usando FPc

--- classificacao/metricas.jl
+++ classificacao/metricas.jl
@@ -11,24 +11,24 @@ function calcular_metricas(modelo,C,x,xo,y_real,beta)
     # 0. Calcular o Hiperplano 
     c = Matrix(C);
     y_modelo = c * w 
-    println("vetor hiperplano =" , y_modelo)  
+    #println("vetor hiperplano =" , y_modelo)  
 
     # 0.1 Tranformar o data frame y_real em vector (importante)
     ##y_real = Matrix(y_real)
     
     # 1.0 Métricas 
 
-    println(".......................................................")
+    
     println("                        Métricas                       ")
-    println(".......................................................") 
+    
     
     y_predito = y_modelo .>= wo 
     y_real .== y_real 
     # y_pred : Estou selecionando todos que estão no hiperplano 
     y_pred    = y_modelo .== wo 
-    println("y_predito = ", y_predito)
-    println("y_real    = ",  y_real)
-    println("y_pred    = ", y_pred )
+    #println("y_predito = ", y_predito)
+    #println("y_real    = ",  y_real)
+    #println("y_pred    = ", y_pred )
      
     # 2. Calculando TP, FN, FP,TN 
     TP = sum((y_real .==1) .& (y_predito .==1))
@@ -49,51 +49,62 @@ function calcular_metricas(modelo,C,x,xo,y_real,beta)
     ERc1 = sum((y_real .==1) .& (y_pred .==1))
     ERc2 = sum((y_real .==0) .& (y_pred .==1))
     Indef = ERc1+ERc2 
-    Soma = TP +FN + FP + TN 
+    Soma = TP +FN + FP + TN
+    
     println("Erro Classe A = ", ERc1)
     println("Erro Classe B = ", ERc2)
     println("Indefindos    = ", Indef)
-    println("Soma = ", Soma)
+    println("Soma          = ", Soma)
+    
 
     # 2.2 Correção do Falso Positivo 
     FPc = FP - ERc2 
+    Soma_C = TP +FN + FPc + TN 
     println("Falso Positivo Correto = ", FPc)
+    println("Soma_C        = ", Soma_C)
 
     # 3. Taxa Positivo acerto/erro  
     TPA = TP  /(TP + FN)
     TPE = FN / (TP + FN)
-    println("Taxa Positivo acerto = ", TPA)
-    println("Taxa Positivo erro  = ",   TPE)
+    TPA = round(TPA, digits=2)
+    TPE = round(TPE, digits=2)
+    #println("Taxa Positivo acerto = ", TPA)
+    #println("Taxa Positivo erro  = ",   TPE)
 
     # 4. Taxa Indefinidos Positivo/Negativo : sobre o hiperplano 
     TIP = ERc1 / (ERc1 + ERc2) 
-    TIN = Erc2 / (ERc1 + ERc2)
-    println("Taxa Positivo acerto = ",  TIP)
-    println("Taxa Positivo erro  = ",   TIN) 
+    TIN = ERc2 / (ERc1 + ERc2)
+    #println("Taxa Positivo acerto = ",  TIP)
+    #println("Taxa Positivo erro  = ",   TIN) 
+    TIP = round(TIP, digits=2)
+    TIN = round(TIN, digits=2)
 
      # 5. Taxa Negativo Acerto Erro  
-     TNA = TN  /(TN + FP)
-     TNE = FP / (FP + TN)
-     println("Taxa Positivo acerto = ", TNA)
-     println("Taxa Positivo erro  = ",   TNE)
+     TNA = TN  /(TN + FPc)   # Falso Positivo C 
+     TNE = FPc / (FPc + TN)
+     TNA = round(TNA, digits=2)
+     TNE = round(TNE, digits=2)
+     #println("Taxa Positivo acerto = ", TNA)
+     #println("Taxa Positivo erro  = ",   TNE)
 
 
     println("............................................")
-    println("     Taxa de Acerto  |  Taxa de Erro ")
-    println(" Positivo  : ",   TPA, " |       " , TPE)
-    println("Indefinido P/N  : ",   TIP, " |       " , TIN)
-    println(" Negativo : ",   TNA, " |       " , TNE)
+    println("    Taxa de Acerto |  Taxa de Erro ")
+    println(" Positivo   : ",   TPA, "   |    " , TPE)
+    println(" Indefinido : ",   TIP, "  |    " , TIN)
+    println(" Negativo   : ",   TNA, "  |    " , TNE)
+    println("............................................")
 
 
 
     
     # 4 Medidas de precisão 
     # Calcular a Acurácia (Optei por retirar FPc da acc e prec)
-    accuracy = (TP + TN) / (TP + FN + FP +TN)
+    accuracy = (TP + TN) / (TP + FN + FPc +TN)
     accuracy = round(accuracy, digits=2)
 
     # Calculando precisão de Recall 
-    precision = TP / (TP + FP)
+    precision = TP / (TP + FPc)
     recall = TP / (TP + FN)
     precision = round(precision, digits=2)
     recall = round(recall, digits=2)
@@ -113,6 +124,7 @@ function calcular_metricas(modelo,C,x,xo,y_real,beta)
     FO = objective_value(modelo)
     status = termination_status(modelo)
     time = solve_time(modelo)
+    time = round(time,digits=4)
     (m,n) = size(C);
     n1 = size(ca,1);
     n2 = size(cb,1)
@@ -126,53 +138,47 @@ function calcular_metricas(modelo,C,x,xo,y_real,beta)
     println("Status = ", status)
     println("Time  = ", time)
     println("Hiperlano       = ",  wo)
-    println("---------Impressão dados de Entrada--------")
-    println("Alpha = ", alpha)
-    println("Conj. treino (n)  = ", m)
-    println("Dados de Teste    = ", n1+n2)
-    println("Positivos CA (n1) = ", n1)
-    println("Negativos CB (n2) = ", n2)
-    println("------------------------------------------------")
+    
   
-    println("----------------------------")
-    println("Erro Classe A = ", ERc1)
-    println("Erro Classe B = ", ERc2)
-    println("Indefindos    = ", Indef)
-    println("Falso Positivo Correto = ", FPc)
-    println("----------------------------")
-     println("----------------------------")
-    println("Acurácia    =  ", accuracy)
-    println("precision   =  " , precision)
-    println("recall      =  ", recall)
-    println("F1Score     =  ", f1_score)
-    println("========================================================")
-    # Salvando os resultados em um arquivo de texto
-    open("resultados_teste1.txt", "w") do file
-    write(file," =============================================================== \n")
-    write(file,"                     Teste do Modelo GP_1 : A             \n")
-    write(file," =============================================================== \n")
-    write(file," \n")
-    write(file,"Função Objetivo = $FO\n")
-    write(file,"hiperplano      = $xo\n")
-    write(file,"---------------------------------------------------------------\n")
-    write(file,                        "Matriz de Confusão                     \n")
-    write(file,"--------------------------------------------------------------- \n")   
-    write(file, "|| True Positive (TP) = $TP  | False Negative (FN) = $FN ||\n")
-    write(file, "|| False Positive (FP) = $FP | True Negative (TN) = $TN  ||\n")
-    write(file,"--------------------------------------------------------------- \n")   
-    write(file,"-----------------------------\n")
-    write(file,"Erro Classe A =  $ERc1\n")
-    write(file, "Erro Classe B =  $ERc2\n")
-    write(file,"Indefindos    =  $Indef\n") 
-    write(file,"Falso Positivo Correto = $FPc\n")
-    write(file,"----------------------------\n")
-    write(file,"-----------------------------\n")
-    write(file, "accuray     = $accuracy\n")
-    write(file,"precision   = $precision\n") 
-    write(file,"recall      = $recall\n") 
-    write(file, "F1Score     = $f1_score\n")
-    write(file,"*****************************************************\n")
-  end 
+  # Salvar em um arquivo TXT
+    # nome do arquivo 
+    filename = "tabela_gp1.txt"
+    # abre o arquivo para a escrita 
+    open(filename, "w") do file 
+        println(file, "========================================")
+        println(file, "Tabela de Resultado das Taxas - GP 1: A ")
+        println(file, "========================================")
+        @printf(file, "Função Objetivo  = %.2f\n", FO)
+        println(file,"Status = ", status)
+        println(file,"Time  = ", time)
+        println(file, "-----------------------------------------")
+        println(file,"--------------------------------------------------------------")
+        println(file,"                      Matriz de Confusão                        ")
+        println(file,"--------------------------------------------------------------")
+        println(file,"|| True Positive  (TP)  = ", TP, " | False Negative (FN)  = ", FN," ||")
+        println(file,"|| False Positive (FP)  = ", FP, " | True Negative  (TN) =  ", TN," ||") 
+        println(file,"---------------------------------------------------------------")
+        println(file,"Erro Classe A = ", ERc1)
+        println(file,"Erro Classe B = ", ERc2)
+        println(file,"Indefindos    = ", Indef)
+        println(file,"Soma          = ", Soma)
+        println(file,"Falso Positivo Correto = ", FPc)
+        println(file,"Soma_C        = ", Soma_C)
+        println(file,"............................................")
+        println(file,"   Taxa de Acerto  |  Taxa de Erro ")
+        println(file," Positivo   : ",   TPA, "  |    " , TPE)
+        println(file," Indefinido : ",   TIP, " |    " , TIN)
+        println(file," Negativo   : ",   TNA, " |    " , TNE)
+        println(file,"............................................")
+        println(file,"Acurácia    =  ", accuracy)
+        println(file,"precision   =  " , precision)
+        println(file,"recall      =  ", recall)
+        println(file,"F1Score     =  ", f1_score)
+        println(file,"********************************************")
+    end 
+
+
+  
     
 end
 
