b61765a4bc80d85ca0cac40cc130c98e428ed315
Ricardo <ricardosoli85@gmail.com>
2024-08-26 11:43:04 -0300

    codigo até optimzie modelo

--- robustaclassificacao/RGP_M1.jl
+++ robustaclassificacao/RGP_M1.jl
@@ -10,29 +10,49 @@ using Gurobi
 #  Modelo de programação de Metas #
 #======================================================#
 
-function gp_rob_1(C,ca,cb,alpha)
-    # criar o modelo
-    modelo = Model(Gurobi.Optimizer)
-    
-    # numero de variaveis
-    n = length(c); 
-    m,n = size(A); 
+function gp_rob_1(C,ca,cb,ca_hat,cb_hat,alpha,gama)
+       
+    # numero de variáveis
+    (n,m) = size(C); 
+    n1 = size(ca,1);
+    n2 = size(cb,1); 
 
     # parametros 
+    alpha = alpha; 
+    gama = gama; 
+
+    # criando duas variantes de gama
+    gama_a = gama[1:n1]; 
+    gama_b = gama[1:n2]; 
+
+    # criar o modelo
+    modelo = JuMP.Model(Gurobi.Optimizer)
     
-    # variaveis
-    @variable(modelo, x[1:n]>=0)
-    @variable(modelo, pos[1:m]>=0)
-    @variable(modelo, neg[1:m]>=0)
-    @variable(modelo, p[1:m,1:n]>=0)
-    @variable(modelo, z[1:m]>=0)
+    # Variaveis
+    @variable(modelo, 0 <= x[1:n] <=1) 
+    @variable(modelo, xo_target)              
+    @variables(modelo,                
+    begin
+        pos_a[1:n1] >= 0 
+        pos_b[1:n2] >= 0
+        neg_a[1:n1] >= 0 
+        pos_b[1:n2] >= 0
+    end)
+    # variáveis do modelo Robusto
+    @variables(modelo, 
+    begin
+        p_a[1:n1,1:m] >= 0 
+        p_b[1:n2,1:m] >= 0 
+        z_a[1:n1]     >= 0
+        z_b[1:n2]     >= 0
+    end)
     
    
 
     # função Objetivo
-    @objective(modelo,Min, sum(c[i]*pos[i] for i in 1:m))
+    @objective(modelo,Min, sum(neg_a[i] for i=1:n1) + sum(pos_b[i] for i=1:n2))
 
-    # restrições
+    # Restrições
     @constraint(modelo, rest1[i in 1:m], 
     sum(A[i,j]*x[j] for j in 1:n)+ 
     sum(p[i,j] for j in 1:n)- 
@@ -40,11 +60,28 @@ function gp_rob_1(C,ca,cb,alpha)
     neg[i] + 
     gama[i]*z[i] ==b[i])
 
-    @constraint(modelo, rest2[i in 1:m,  j in 1:n],
-    z[i] + p[i,j] >=   a[i,j]*x[j])
- 
-    
- 
+    # restrições 
+    @constraints(modelo, 
+    begin
+    rest1[i=1:n1], sum(ca[i,j]*x[j] for j in 1:m) + 
+    sum(p_a[i,j] for j in 1:m) + 
+    neg_a[i] - pos_a[i] + 
+    gama_a[i] * z_a[i] == xo_target
+    rest2[i=1:n2], sum(cb[i,j]*x[j] for j in 1:m)+
+    sum(p_b[i:j] for j in 1:m) +
+    neg_b[i] - pos_b[i] + 
+    gama_b[i] * z_b[i] == xo_target  
+    end
+    )
+
+    @constraints(modelo, 
+    begin
+        rest3[i in 1:n1, j in 1:m], z_a[i] + p_a[i,j] >= ca_hat[i,j] * x[j]
+        rest4[i in 1:n2, j in 1:m], z_b[i] + p_b[i,j] >= cb_hat[i,j] * x[j]
+    end)
+
+    @constraint(modelo, rest5[1:m], sum(x[j] for j in 1:m) ==1)
+   
     # resolver 
     optimize!(modelo)
 
